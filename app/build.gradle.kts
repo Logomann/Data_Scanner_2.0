b6c6242df23cda9f8cffcf90fec80b261b65e923
Vitaliy Molodanov <53826004+Logomann@users.noreply.github.com>
2024-08-31 17:56:57 +0300

    1.0.3 bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.logomann.datascanner20"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 5
         versionName = "1.0.3"
 
